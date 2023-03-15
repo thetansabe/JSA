28a3464a6c81a1bacc25478aaaf46930e774fd4b
thetansabe <thetannguyen193@gmail.com>
2023-03-15 16:54:03 +0700

    search + loading

--- script.js
+++ script.js
@@ -1,4 +1,89 @@
 import {flowers, teddies} from "./data.js"
 
-console.log(flowers);
-console.log(teddies)
+// console.log(flowers)
+// console.log(teddies)
+
+function render(flowers, teddies){
+    const flowerSection = document.getElementById('flower')
+    const teddySection = document.getElementById('teddy')
+
+    flowerSection.innerHTML = ''
+    teddySection.innerHTML = ''
+
+    //render flowers
+    for(let flower of flowers){
+        const content = `
+            <a class="item" href="./detail.html">
+                <div class="item_img">
+                <img
+                    src="${flower.img}"
+                    alt=""
+                />
+                </div>
+                <div class="item_content">
+                <h3>${flower.name}</h3>
+                <p>Price : ${flower.price}</p>
+                </div>
+            </a>
+        `
+
+        flowerSection.innerHTML += content
+    }
+
+    //render teddies
+    for(let teddy of teddies){
+        const content = `
+            <a class="item" href="./detail.html">
+                <div class="item_img">
+                <img
+                    src="${teddy.img}"
+                    alt=""
+                />
+                </div>
+                <div class="item_content">
+                <h3>${teddy.name}</h3>
+                <p>Price : ${teddy.price}</p>
+                </div>
+            </a>
+        `
+
+        teddySection.innerHTML += content
+    }
+}
+
+//default render
+render(flowers, teddies)
+
+//search
+document.getElementById('search').addEventListener('input', e => {
+    const targetProduct = e.target.value
+
+    const newFlowers = removeItem(targetProduct, flowers)
+    const newTeddies = removeItem(targetProduct, teddies)
+
+    //debounce loading
+    document.getElementById('flower')
+        .innerHTML = `<div class="loading"></div>`
+
+    document.getElementById('teddy')
+        .innerHTML = `<div class="loading"></div>`
+
+    setTimeout(() => {
+        render(newFlowers, newTeddies)
+    }, 1000)
+})
+
+//remove item by product name
+function removeItem(productName, products){
+    const arr = []
+    
+    for(let product of products){
+        if(product.name.toLowerCase().includes(productName.toLowerCase())){
+            console.log(product.name)
+            console.log(productName)
+            arr.push(product)
+        }
+    }
+
+    return arr
+}
