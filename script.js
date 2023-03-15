0bf91699b36a0e86fcc96231deebe0d060a6ef09
thetansabe <thetannguyen193@gmail.com>
2023-03-15 22:34:44 +0700

    real debounce

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-import {flowers, teddies} from "./data.js"
+import { flowers, teddies } from "./data.js";
 
 // console.log(flowers)
 // console.log(teddies)
 
-function render(flowers, teddies){
-    const flowerSection = document.getElementById('flower')
-    const teddySection = document.getElementById('teddy')
+function render(flowers, teddies) {
+  const flowerSection = document.getElementById("flower");
+  const teddySection = document.getElementById("teddy");
 
-    flowerSection.innerHTML = ''
-    teddySection.innerHTML = ''
+  flowerSection.innerHTML = "";
+  teddySection.innerHTML = "";
 
-    //render flowers
-    for(let flower of flowers){
-        const content = `
+  //render flowers
+  for (let flower of flowers) {
+    const content = `
             <a class="item" href="./detail.html">
                 <div class="item_img">
                 <img
@@ -25,14 +25,14 @@ function render(flowers, teddies){
                 <p>Price : ${flower.price}</p>
                 </div>
             </a>
-        `
+        `;
 
-        flowerSection.innerHTML += content
-    }
+    flowerSection.innerHTML += content;
+  }
 
-    //render teddies
-    for(let teddy of teddies){
-        const content = `
+  //render teddies
+  for (let teddy of teddies) {
+    const content = `
             <a class="item" href="./detail.html">
                 <div class="item_img">
                 <img
@@ -45,45 +45,81 @@ function render(flowers, teddies){
                 <p>Price : ${teddy.price}</p>
                 </div>
             </a>
-        `
+        `;
 
-        teddySection.innerHTML += content
-    }
+    teddySection.innerHTML += content;
+  }
 }
 
 //default render
-render(flowers, teddies)
+render(flowers, teddies);
 
 //search
-document.getElementById('search').addEventListener('input', e => {
-    const targetProduct = e.target.value
+// document.getElementById("search").addEventListener("input", (e) => {
+//   //debounce loading
+//   document.getElementById("flower").innerHTML = `<div class="loading"></div>`;
 
-    const newFlowers = removeItem(targetProduct, flowers)
-    const newTeddies = removeItem(targetProduct, teddies)
+//   document.getElementById("teddy").innerHTML = `<div class="loading"></div>`;
 
-    //debounce loading
-    document.getElementById('flower')
-        .innerHTML = `<div class="loading"></div>`
+//   setTimeout(() => {
+//     const targetProduct = e.target.value;
+//     console.log(targetProduct);
+//     const newFlowers = removeItem(targetProduct, flowers);
+//     const newTeddies = removeItem(targetProduct, teddies);
+//     render(newFlowers, newTeddies);
+//   }, 1000);
+// });
 
-    document.getElementById('teddy')
-        .innerHTML = `<div class="loading"></div>`
-
-    setTimeout(() => {
-        render(newFlowers, newTeddies)
-    }, 1000)
-})
+document.getElementById("search")
+    .addEventListener(
+        "input", 
+        debounce(handleSearch, 2000)
+    )
 
 //remove item by product name
-function removeItem(productName, products){
-    const arr = []
-    
-    for(let product of products){
-        if(product.name.toLowerCase().includes(productName.toLowerCase())){
-            console.log(product.name)
-            console.log(productName)
-            arr.push(product)
-        }
-    }
+function removeItem(productName, products) {
+  const arr = [];
 
-    return arr
+  for (let product of products) {
+    if (product.name.toLowerCase().includes(productName.toLowerCase())) {
+    //   console.log(product.name);
+    //   console.log(productName);
+      arr.push(product);
+    }
+  }
+
+  return arr;
+}
+
+// handle onChange
+function handleSearch(e){
+    const targetProduct = e.target.value;
+    console.log(targetProduct);
+    const newFlowers = removeItem(targetProduct, flowers);
+    const newTeddies = removeItem(targetProduct, teddies);
+    render(newFlowers, newTeddies);
+}
+
+function loading(){
+    document.getElementById("flower").innerHTML = `<div class="loading"></div>`;
+    document.getElementById("teddy").innerHTML = `<div class="loading"></div>`;
+}
+
+//real debounce
+function debounce(fn, sleep){
+    let timer = null
+
+    return function(...args){
+        loading()
+        
+        if(timer) {
+            clearTimeout(timer)
+            timer = null
+        }
+
+        timer = setTimeout(() => {
+            fn.apply(this, args)
+            timer = null
+        }, sleep)
+    }
 }
